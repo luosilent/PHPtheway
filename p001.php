fb976356d6b0aa7c5ad3e9aa0f993ce2f9db9327
luosilnet <luosilnet@163.com>
2018-05-24 10:51:23 +0800

    chapter01面向对象编程

--- p001.php
+++ p001.php
@@ -7,7 +7,9 @@
  * parent关键字用来指向父类，所以可以使用该关键字调用父类的属性和方法。
  * $this变量用来在类体内调用自身的属性和方法。
  */
+
 /**
+ * 基本概念
  * 例1
  * Class SimpleClass
  */
@@ -77,6 +79,7 @@ echo "<br>";
 B::bar();
 echo "<hr>";
 /**
+ * 属性
  * 在类的成员方法里面，可以用 ->（对象运算符）：$this->property（其中 property 是该属性名）
  * 这种方式来访问非静态属性。静态属性则是用 ::（双冒号）：self::$property 来访问。
  * 更多静态属性与非静态属性的区别参见 Static 关键字。
@@ -105,6 +108,10 @@ EOD;
     // 跟 heredocs 不同，nowdocs 可在任何静态数据上下文中使用，包括属性声明。
 }
 
+/**
+ * 类常量
+ * Class MyClass
+ */
 class MyClass
 {
     const constant = 'constant value';
@@ -131,6 +138,7 @@ echo $class::constant."\n"; // 自 PHP 5.3.0 起
 echo "<hr>";
 
 /**
+ * 类的自动加载
  *  spl_autoload_register() 函数可以注册任意数量的自动加载器，
  *  当使用尚未被定义的类（class）和接口（interface）时自动去加载。
  *  通过注册自动加载器，脚本引擎在 PHP 出错失败前有了最后一个机会加载所需的类。
@@ -160,7 +168,9 @@ try {
 } catch (Exception $e) {
     echo $e->getMessage(), "\n";
 }*/
-
+/**
+ * 构造函数和析构函数
+ */
 /**
  * PHP 5 允行开发者在一个类中定义一个方法作为构造函数。
  * 具有构造函数的类会在每次创建新对象时先调用此方法，所以非常适合在使用对象之前做一些初始化工作。
@@ -195,3 +205,170 @@ $obj = new SubClass();
 
 // In BaseClass constructor
 $obj = new OtherSubClass();
+echo "<hr>";
+
+/**
+ * PHP 5 引入了析构函数的概念，这类似于其它面向对象的语言，
+ * 如 C++。析构函数会在到某个对象的所有引用都被删除或者当对象被显式销毁时执行。
+ */
+/**
+ * 使用和理解多个构造的简单方法
+ */
+
+class ABC
+{
+    function __construct()
+    {
+        $a = func_get_args();
+        $i = func_num_args();
+        if (method_exists($this,$f='__construct'.$i)) {
+            call_user_func_array(array($this,$f),$a);
+        }
+    }
+
+    function __construct1($a1)
+    {
+        echo('__construct with 1 param called: '.$a1.PHP_EOL);
+    }
+
+    function __construct2($a1,$a2)
+    {
+        echo('__construct with 2 params called: '.$a1.','.$a2.PHP_EOL);
+    }
+
+    function __construct3($a1,$a2,$a3)
+    {
+        echo('__construct with 3 params called: '.$a1.','.$a2.','.$a3.PHP_EOL);
+    }
+}
+echo 'p001.6 类的构造函数简单示例';
+echo "<br>";
+$outputABC = new ABC('sheep');
+echo "<br>";
+$outputABC = new ABC('sheep','cat');
+echo "<br>";
+$outputABC = new ABC('sheep','cat','dog');
+echo "<hr>";
+// results:
+// __construct with 1 param called: sheep
+// __construct with 2 params called: sheep,cat
+// __construct with 3 params called: sheep,cat,dog
+
+/**
+ * 类的析构函数的示例
+ * Class MyDestructableClass
+ */
+class MyDestructableClass {
+    function __construct() {
+        print "In constructor\n";
+        $this->name = "MyDestructableClass";
+        echo "<br>";
+    }
+
+    function __destruct() {
+        echo "我总是最后执行的！！";
+        echo "<br>";
+        print "Destroying " . $this->name . "\n";
+
+    }
+}
+echo 'p001.7 类的析构函数的示例';
+echo "<br>";
+$obj = new MyDestructableClass();
+echo "<hr>";
+
+/**
+ * 访问控制（可见性）
+ * 对属性或方法的访问控制，是通过在前面添加关键字 public，protected或 private来实现的。
+ * public（公有）被定义为公有的类成员可以在任何地方被访问。
+ * protected（受保护）被定义为受保护的类成员则可以被其自身以及其子类和父类访问。
+ * private（私有）被定义为私有的类成员则只能被其定义所在的类访问。
+ */
+
+/**
+ * 例子
+ * Class Item
+ */
+class Item
+{
+    protected $label = 'Unknown Item'; // Rule 1 - protected.
+    protected $price = 0.0;            // Rule 1 - protected.
+
+    public function getLabel() {       // Rule 2 - public function.
+        return $this->label;             // Rule 3 - string OUT for $label.
+    }
+
+    public function getPrice() {       // Rule 2 - public function.
+        return $this->price;             // Rule 4 - float OUT for $price.
+    }
+
+    public function setLabel($label)   // Rule 2 - public function.
+    {
+
+        if(is_string($label))
+        {
+            $this->label = (string)$label; // Rule 3 - string IN for $label.
+        }
+    }
+
+    public function setPrice($price)   // Rule 2 - public function.
+    {
+
+        if(is_numeric($price))
+        {
+            $this->price = (float)$price; // Rule 4 - float IN for $price.
+        }
+    }
+    public function __toString()
+    {
+        return "label:"."$this->label  "."$this->price".'$';
+    }
+}
+$item = new Item();
+$item->setLabel( 'apples');
+$item->setPrice(49.99);
+print $item;
+echo "<hr>";
+
+/**
+ * 对象继承
+ * 继承已为大家所熟知的一个程序设计特性，PHP 的对象模型也使用了继承。
+ * 继承将会影响到类与类，对象与对象之间的关系。比如，当扩展一个类，子类就会继承父类所有公有的和受保护的方法。
+ * 除非子类覆盖了父类的方法，被继承的方法都会保留其原有功能。
+ */
+
+class foo
+{
+    public function printItem($string)
+    {
+        echo 'Foo: ' . $string . PHP_EOL;// 文本换行，windows平台相当于echo "\r\n";
+    }
+
+    public function printPHP()
+    {
+        echo 'PHP is great.' . PHP_EOL;
+    }
+}
+
+class bar extends foo
+{
+    public function printItem($string)
+    {
+        echo 'Bar: ' . $string . PHP_EOL;
+    }
+}
+
+$foo = new foo();
+$bar = new bar();
+$foo->printItem('baz'); // Output: 'Foo: baz'
+echo "<br>";
+$foo->printPHP();       // Output: 'PHP is great'
+echo "<br>";
+$bar->printItem('baz'); // Output: 'Bar: baz'
+echo "<br>";
+$bar->printPHP();       // Output: 'PHP is great'
+echo "<hr>";
+
+/**
+ * 范围解析操作符 （::）
+ */

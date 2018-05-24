f6c8367701487c926de1372692b0fdb5212fb506
luosilnet <luosilnet@163.com>
2018-05-24 09:17:19 +0800

    chapter01面向对象编程

--- p001.php
+++ p001.php
@@ -76,3 +76,93 @@ echo "<br>";
 
 B::bar();
 echo "<hr>";
+/**
+ * 在类的成员方法里面，可以用 ->（对象运算符）：$this->property（其中 property 是该属性名）
+ * 这种方式来访问非静态属性。静态属性则是用 ::（双冒号）：self::$property 来访问。
+ * 更多静态属性与非静态属性的区别参见 Static 关键字。
+ */
+
+class SimpleClass2
+{
+    // 错误的属性声明
+    /*  public $var1 = 'hello ' . 'world';
+        public $var2 = <<<EOD
+    hello world
+    EOD;
+        public $var3 = 1+2;
+        public $var4 = self::myStaticMethod();
+        public $var5 = $myVar;*/
+
+
+    // 正确的属性声明
+    public $var6 = myConstant;
+    public $var7 = array(true, false);
+
+    //在 PHP 5.3.0 及之后，下面的声明也正确
+    public $var8 = <<<'EOD'
+hello world
+EOD;
+    // 跟 heredocs 不同，nowdocs 可在任何静态数据上下文中使用，包括属性声明。
+}
+
+class MyClass
+{
+    const constant = 'constant value';
+
+    function showConstant() {
+        echo  self::constant . "\n";
+    }
+}
+echo 'p001.3 类常量的示例';
+echo "<br>";
+
+echo MyClass::constant . "\n";
+echo "<br>";
+
+$classname = "MyClass";
+echo $classname::constant . "\n"; // 自 5.3.0 起
+echo "<br>";
+
+$class = new MyClass();
+$class->showConstant();
+echo "<br>";
+
+echo $class::constant."\n"; // 自 PHP 5.3.0 起
+echo "<hr>";
+
+/**
+ *  spl_autoload_register() 函数可以注册任意数量的自动加载器，
+ *  当使用尚未被定义的类（class）和接口（interface）时自动去加载。
+ *  通过注册自动加载器，脚本引擎在 PHP 出错失败前有了最后一个机会加载所需的类。
+ *  尽管 __autoload() 函数也能自动加载类和接口，但更建议使用 spl_autoload_register() 函数。
+ */
+
+// 本例尝试分别从 MyClass1.php 和 MyClass2.php 文件中加载 MyClass1 和 MyClass2 类。
+/**
+ * <?php
+ * spl_autoload_register(function ($class_name) {
+ * require_once $class_name . '.php';
+ * });
+ * $obj  = new MyClass1();
+ * $obj2 = new MyClass2();
+ * ?>
+ */
+spl_autoload_register(function ($name) {
+    echo "Want to load $name.\n";
+    echo "<br>";
+    throw new MissingException("Unable to load $name.");
+});
+
+try {
+    echo 'p001.4 类的自动加载的示例';
+    echo "<br>";
+    $obj = new NonLoadableClass();
+} catch (Exception $e) {
+    echo $e->getMessage(), "\n";
+}
+
+/**
+ * PHP 5 允行开发者在一个类中定义一个方法作为构造函数。
+ * 具有构造函数的类会在每次创建新对象时先调用此方法，所以非常适合在使用对象之前做一些初始化工作。
+ */
+

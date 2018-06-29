a6f11c44ea7c891ca3ee4b14d37be38e24985c80
luosilent <luosilent@qq.com>
2018-06-29 15:50:27 +0800

    PHP爬虫

--- spider/phpSpider.php
+++ spider/phpSpider.php
@@ -7,154 +7,130 @@
  */
 header('Content-type:text/html;charset=utf-8');
 
-
-
 $url = $_GET['url'];
 $pattern = $_GET['pattern'];
 
 
-/**
- * Class Spider
- */
-class Spider {
+class Spider
+{
 
-    /**
-     * @param $url
-     * @return bool|string
-     */
+
     public function getContent($url)
     {
         $content = file_get_contents($url);
         return $content;
     }
 
-    /**
-     * @param $content
-     * @param $pattern
-     * @return array
-     */
-    public function extract($content,$pattern){
+
+    public function extract($content, $pattern)
+    {
         $matches = array();
-        preg_match_all($pattern,$content,$matches);
+        preg_match_all($pattern, $content, $matches);
         // preg_match 函数的结果是数组的形式
         return $matches;
     }
 
-    /**
-     * @param $reArr
-     * @param $fen
-     */
-    public function saveInfo($reArr,$fen){
-        $myFile = fopen('result.txt','w') or die ("Unable to open file!");
-        for($i=0;$i<count($reArr);$i++){
-            fwrite($myFile,$reArr[$i].$fen);
+
+    public function saveInfo($reArr, $fen)
+    {
+        $myFile = fopen('result.txt', 'w') or die("Unable to open file!");
+        for ($i = 0; $i < count($reArr); $i ++) {
+            fwrite($myFile, $reArr[$i] . $fen);
         }
         fclose($myFile);
     }
 
-    /**
-     * @param $username
-     * @param $password
-     * @param $dbName
-     */ 
-    public function connect($username,$password,$dbName){
-        $link = mysqli_connect('localhost',$username,$password);
-        $choose = mysqli_select_db($dbName,$link);
-        if (!$choose) {
+
+    public function connect($username, $password, $dbName)
+    {
+        $link = mysqli_connect('localhost', $username, $password);
+        $choose = mysqli_select_db($dbName, $link);
+        if (! $choose) {
             die("Connection failed: " . mysqli_connect_error());
         }
-
     }
 
-
-    /**
-     * @param $link
-     * @param $sql
-     * @return string
-     */
-    public function insetInfo($link,$sql){
-        $query = mysqli_query($link,$sql);
-        if($query && mysqli_affected_rows($link)>0){
+    public function insetInfo($link, $sql)
+    {
+        $query = mysqli_query($link, $sql);
+        if ($query && mysqli_affected_rows($link) > 0) {
             return "数据添加成功";
-        }else{
+        } else {
             return "数据添加失败";
         }
     }
 
-    /**
-     * @param $pattern
-     * @return int
-     */
-    public function countPat($pattern){
-        $reCount = substr_count($pattern,'(.*?)');
+
+    public function countPat($pattern)
+    {
+        $reCount = substr_count($pattern, '(.*?)');
         return $reCount;
     }
 
-    /**
-     * @return array|mixed
-     */
-    public function testDemo(){
+
+    public function testDemo()
+    {
         $url = "http://php.net/manual/zh/funcref.php";
-        $op =  self::getContent($url);
-        $pattern =  '/<a href="(.*?)">(.*?)<\/a>/';
-        $reinFo = self::extract($op,$pattern);
+        $op = self::getContent($url);
+        $pattern = '/<a href="(.*?)">(.*?)<\/a>/';
+        $reinFo = self::extract($op, $pattern);
         $countNum = self::countPat($pattern);
         $reinFo = $reinFo[$countNum];
         return $reinFo;
     }
 
-    /**
-     * @param $url
-     * @param $pattern
-     * @return mixed
-     */
-    public function returnAll($url,$pattern){
-        
+
+    public function returnAll($url, $pattern)
+    {
         $serverName = "localhost";
         $username = "root";
         $password = "root";
         $dbName = "spider";
         $charset = 'utf8';
-
-
+        
         $op = self::getContent($url);
-        $reinFo = self::extract($op,$pattern);
+        $reinFo = self::extract($op, $pattern);
         $countNum = self::countPat($pattern);
         $reinFo = $reinFo[$countNum];
-//        $reinFo = $reinFo[1];
+        // $reinFo = $reinFo[1];
         $conn = new PDO("mysql:host=$serverName;dbname=$dbName", $username, $password);
         $conn->query("set NAMES $charset");
         $i = 0;
         foreach ($reinFo as $key => $value) {
-           
-           // $arr[$i] = $value;
-            // $i++;
-            //$v = $value;
-            // $sql = "INSERT INTO dy (id,user)
-            // VALUES ('".$i."', '".$v."')";
-
-            // $sql = "INSERT INTO dyView (id, view)
-            // VALUES ('".$i."','".$v."')";
-
-            // /<span class="impress-tag-item" .*?[^>]*>(.*?)<\/span>/i
-
-            // $sql = "INSERT INTO dyKey(id, kw)
-            // VALUES ('".$i."','".$v."')";
-
-
-            //echo $sql;
-            echo ($key+1).'.'.$value."<br>";
-            //$sth = $conn->prepare($sql);
-             //$sth->execute();        
-        }
             
-       
-             
-             
-    }
+            // $arr[$i] = $value;
+             $i++;
+             $v = $value;
 
+//            $kk = json_encode($v);print_R($kk);
+//
+//            $ll = json_decode($kk,true);print_R($ll);exit;
+
+             //user正则 /<span class="dy-name ellipsis fl">(.*?)<\/span>/
+             $sql = "INSERT INTO dyUser (id,user)
+             VALUES ('".$i."', '".$v."')";
+            //view正则 /<span class="dy-num fr"  >(.*?)<\/span>/
+             $sql = "INSERT INTO dyUser (id, view)
+             VALUES ('".$i."','".$v."')";
+            
+            //key正则 /<span class="impress-tag-item" .*?[^>]*>(.*?)<\/span>/i
+            
+             $sql = "INSERT INTO dyKey(id, kw)
+             VALUES ('".$i."','".$v."')";
+
+            //            $sql = "INSERT INTO dyUser1 (id, user)
+//             VALUES ('".$i."','".$v."')";
+
+//             $sql = "UPDATE dyUser1 set view = '".$v."'
+//             where id= '".$i."'";
+            
+            // echo $sql;
+            echo ($key + 1) . '.' . $value . "<br>";
+             $sth = $conn->prepare($sql);
+             $sth->execute();
+        }
+    }
 }
 
-
 $crawler = new Spider();
-$re = $crawler -> returnAll($url,$pattern);
+$re = $crawler->returnAll($url, $pattern);
--- spider/spider.php
+++ spider/spider.php
@@ -6,26 +6,130 @@
  * Time: 14:51
  */
 
-// 通过 file_get_contents 函数获取页面源码
-$html = file_get_contents("https://secure.php.net/manual/en/funcref.php");
+header('Content-type:text/html;charset=utf-8');
 
-// 通过 preg_replace 函数使页面源码由多行变单行
-$htmlOneLine = preg_replace("/\r|\n|\t/","",$html);
+$url = $_GET['url'];
+$pattern = $_GET['pattern'];
 
-// 通过 preg_match 函数提取获取页面的标题信息
-//preg_match("/<title>(.*)<\/title>/iU",$htmlOneLine,$titleArr);
-preg_match("/<\/a><\/dt>(.*)<\/dl>/iU",$htmlOneLine,$linkArr);
-preg_match("/<\/a><\/dt><dd><a href='(.*?)'>/iU",$htmlOneLine,$links);
 
-// 由于 preg_match 函数的结果是数组的形式
-//$title = $titleArr[1];
-//foreach ($linkArr as $item){
-//    echo $item;
-//}
-foreach ($links as $link){
-    echo $link;
+class LuoSpider
+{
+
+
+    public function getContent($url)
+    {
+        $content = file_get_contents($url);
+
+        return $content;
+    }
+
+
+    public function extract($content, $pattern)
+    {
+        $matches = array();
+        preg_match_all($pattern, $content, $matches);
+
+        // preg_match 函数的结果是数组的形式
+        return $matches;
+    }
+
+    public function conn()
+    {
+
+        $host = '127.0.0.1';
+        $dbName = 'spider';
+        $charset = 'utf8';
+        $dsn = "mysql:host=$host;dbName=$dbName;charset=$charset";
+        $uerName = "root";
+        $password = "root";
+
+        try {
+            $conn = new PDO($dsn, $uerName, $password);
+            $conn->query("set NAMES $charset");
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //添加使用数据库
+            $str = "use $dbName";
+            $conn->exec($str);
+        } catch (PDOException $e) {
+            $error = "Access denied! ";
+            $ext = $e->getMessage();
+            echo $error . $ext;
+        }
+
+        return $conn;
+
+    }
+
+
+    public function countPat($pattern)
+    {
+        $reCount = substr_count($pattern, '(.*?)');
+
+        return $reCount;
+    }
+
+
+    public function returnAll($url, $pattern)
+    {
+
+        $conn = self:: conn();
+        $op = self::getContent($url);
+        $reinFo = self::extract($op, $pattern);
+        $countNum = self::countPat($pattern);
+        $reinFo = $reinFo[$countNum];
+//         $reinFo = $reinFo[1];
+        $i = 0;
+        foreach ($reinFo as $key => $value) {
+
+            $arr[$i] = $value;
+            $i++;
+            //$arr1 = array_chunk($arr, 4);
+
+            //echo ($key + 1) . '.' . $value . "<br>";
+
+//            存入user
+            //user正则 /<span class="dy-name ellipsis fl">(.*?)<\/span>/
+            // $sql = "INSERT INTO dyUser1 (id, user)
+            // VALUES ('".$i."','".$v."')";
+
+//            更新user
+            $sql = "UPDATE `dyUser1` set `user` = '" . $value . "'
+              where id= '" . $i . "'";
+
+//            更新view
+            //view正则 /<span class="dy-num fr"  >(.*?)<\/span>/
+//            $sql = "UPDATE dyUser1 set view = '".$value."'
+//              where id= '".$i."'";
+
+//            更新keyWord
+            //key正则 /<span class="impress-tag-item" .*?[^>]*>(.*?)<\/span>/i
+            //$sql = "UPDATE dyUser1 set key = '" . $value . "'
+            //   where id = '" . $i . "'";
+//            echo ($key + 1) . '.' . $value . "<br>";
+//            //echo $sql;exit;
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+
+        }
+//        $i = 0;
+//        foreach ($arr1 as $k => $dd) {
+//            foreach ($dd as $d) {
+//                $dd_arr[] = $d;
+//            }
+//            $list_dd = join(',', $dd_arr);
+//            unset($dd_arr);
+//            $i++;
+//            $sql = "UPDATE `dyUser1` set `key` = '" . $list_dd . "'
+//            where id = '" . $i . "'";
+//            echo $sql;
+//            $stmt = $conn->prepare($sql);
+//            $stmt->execute();
+//        }
+
+    }
 }
 
-// 通过 echo 函数输出标题信息
-//echo $title;
+$crawler = new LuoSpider();
+$re = $crawler->returnAll($url, $pattern);
+
 

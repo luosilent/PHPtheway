78eb98d1579afe46f0d0d7e48da61afc577e93b9
luosilent <luosilent@qq.com>
2018-07-20 16:45:39 +0800

    curl - 伪造ip天气查询

--- getWeather/getWeather.php
+++ getWeather/getWeather.php
@@ -9,10 +9,9 @@
 header('Access-Control-Allow-Origin:*');//跨域
 $theCityCode = addslashes($_POST["city"]);
 $data = "theCityCode=$theCityCode&theUserID=";
-//$data = 'theCityCode=杭州&theUserID=';默认杭州
 $ch = curl_init();
 $ip = "100.100." . rand(1, 255) . "." . rand(1, 255);
-//echo $ip;加上伪造ip没有24小时点击限制、美滋滋
+//echo $ip;//加上伪造ip没有24小时点击限制、美滋滋
 curl_setopt($ch, CURLOPT_URL, "http://www.webxml.com.cn/WebServices/WeatherWS.asmx/getWeather");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POST, 1);

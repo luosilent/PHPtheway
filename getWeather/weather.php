3380a2ecbaee6b434c9b147febaa7b95f56cf591
luosilent <luosilent@163.com>
2018-07-28 20:57:25 +0800

    php - 天气查询

--- getWeather/weather.php
+++ getWeather/weather.php
@@ -1,65 +1,90 @@
 <!--by luosilent-->
-<?php
-require_once "getCity.php";
-$ip = $_SERVER['REMOTE_ADDR'];
-$address = GetIpFrom($ip);
-//print_r($address);
-$arr = json_decode($address);
-$city=$arr->data->city;
-$dip=iconv("utf-8","gb2312",$arr->data->ip);
-print_r( "当前IP为 :".$dip);
-if ($city == "内网IP"){
-    $city="未知城市";
-    print_r("当前城市为:未知");
-}else{
-    print_r("当前城市为:".$city);
-}
-
-
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=gb2312"/>
+    <meta http-equiv="Content-Type"  name="viewport" content="width=device-width, charset=gb2312, initial-scale=1.0"/>
     <title>luosilent天气查询</title>
-    <script type="text/javascript" src="http://libs.baidu.com/jquery/2.1.4/jquery.min.js"></script>
-    <script language="javascript">
-        function getWeather() {
-            if ($('#city').val() == "") {
-                $('#msg').html("请输入城市名称再查询!");
-                $('#city').focus;
-                return false;
-            }
-            ajax_post();
-        }
-
-        function ajax_post() {
-            $.post("getWeather.php", {city: $('#city').val()},
-                function (data) {
-                    //$('#msg').html("please enter the city!");
-                    // alert(data);
-                    $('#msg').html(data);
-                },
-                "text");//这里返回的类型有：json,html,xml,text
-        }
-    </script>
+    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
+<div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <!-- 确保无论是宽屏还是窄屏，navbar-brand都显示 -->
+        <a class="navbar-brand" href="http://localhost/php/PHPtheway/liu/index.html">Luosilent</a>
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-responsive-collapser">
+            <li><a href="http://localhost/php/PHPtheway/getWeather/weather.php">天气查询</a></li>
+            <li><a href="http://localhost/php/PHPtheway/liu/index.html">天气查询</a></li>
+            <li><a href="http://bbs.luosilent.top/lcg/index.php">开奖查询</a></li>
+            <li><a href="#about">待开发</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    个人网站
+                    <b class="caret"></b>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="http://luosilent.top/">博客</a></li>
+                    <li><a href="http://bbs.luosilent.top/">论坛</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="container"> 
+    <div class="row">
+        <div class="col-xs-12 col-md-12 col-lg-12">
+            <form role="form" id="ajaxform" name="ajaxform" method="post" action="getWeather.php">
+                <div class="form-group">
+                    <div style="margin-top: 35px;">
+                        <label>天气查询:</label>
+                        <input class="input " style="width:100%;margin-bottom: 10px" name="city" id="city" value="云霄"
+                               placeholder="请输入城市">
+                    </div>
 
-<form style="text-align:center;font-size:2rem;" id="ajaxform" name="ajaxform" method="post" action="getWeather.php">
-        <p>
-            请输入城市名称：<input style="font-size:2rem;" type="text" name="city" id="city" value="<?=$city?>" size="10"/>
-        </p>
-        <p>
-            <input style="display:none"><!--防止回车键自动提交表单-->
-        </p>
+                    <div>
+                        <input style="display:none"><!--防止回车键自动提交表单-->
+                    </div>
+                    <input class="btn btn-primary" style="width:100%;margin-top: 10px" name="submit" type="button"
+                           value="天气查询"
+                           onclick="return getWeather()"/>
+                    <div style="margin-top: 20px">
+                        <p id="msg"></p>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 
-        <p>
-            <input style="font-size:2rem;" name="submit" type="button" value="天气查询" onclick="return getWeather()"/>
-        </p>
-        <p id="msg"></p>
-</form>
+<script language="javascript">
+    function getWeather() {
+        if ($('#city').val() == "") {
+            $('#msg').html("请输入城市名称再查询!");
+            $('#city').focus;
+            return false;
+        }
+        ajax_post();
+    }
+
+    function ajax_post() {
+        $.post("getWeather.php", {city: $('#city').val()},
+            function (data) {
+                //$('#msg').html("please enter the city!");
+                // alert(data);
+                $('#msg').html(data);
+            },
+            "text");//这里返回的类型有：json,html,xml,text
+    }
+</script>
 </body>
 </html>

888217ffc709b554a773232049f364f8a507f3db
luosilent <luosilent@qq.com>
2018-08-15 09:14:34 +0800

    php - 修改json

---
+++ p011.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luosilent
+ * Date: 2018/8/15
+ * Time: 9:14
+ */
+
+$json_string = file_get_contents("regions.json");// 从文件中读取数据到PHP变量
+$data = json_decode($json_string,true);// 把JSON字符串转成PHP数组
+foreach ($data as &$d) {
+    $d['code'] = substr($d['code'], 0, 2);
+    if(is_array($d['cities'])){
+        foreach ($d['cities'] as &$lc) {
+            $lc['code'] = substr($lc['code'], 0, 4);
+        }
+    }
+}
+
+$json_strings = json_encode($data);
+
+file_put_contents("text3.json",$json_strings);//写入

c0c574d3078e0caac5a5b8240e2e5b15207a7e3d
luosilent <luosilent@qq.com>
2018-09-06 16:43:11 +0800

    php

--- stu/p013.php
+++ stu/p013.php
@@ -2,12 +2,20 @@
 /**
  * Created by PhpStorm.
  * User: luosilent
- * Date: 2018/8/18
- * Time: 9:38
+ * Date: 2018/9/6
+ * Time: 16:15
  */
-//直接读取json文件，也可以从数据库读取
-$json_string = file_get_contents("regions.json");// 从文件中读取数据到PHP变量
-//$data = json_decode($json_string,true);// 把JSON字符串转成PHP数组
-//$json_strings = json_encode($data);
 
-return $json_string;
+
+
+$password = "qwe123";
+// 计算密码的哈希值。$hashedPassword 是一个长度为 60 个字符的字符串.
+$hashedPassword = password_hash($password,PASSWORD_DEFAULT);
+// 你现在可以安全地将 $hashedPassword 保存到数据库中!
+echo $hashedPassword;
+echo "<br>";
+//echo password_hash("qwe123", PASSWORD_DEFAULT);
+//密码验证
+if(password_verify($password, $hashedPassword)) {
+    echo 'same';
+}
